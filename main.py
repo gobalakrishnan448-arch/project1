724f68852e03c7f09f74394064aabba8d7cb49c5
Gobala <your_email@gmail.com>
2026-03-04 21:15:43 +0530

    database connected

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 import psycopg2
 import os
+from sqlalchemy import create_engine, text
 
 app = FastAPI()
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
 
-@app.get("/")
-def home():
-    return {"message": "TNEA Prediction System Running"}
+@app.get("/test-db")
+def test_db():
+    with engine.connect() as connection:
+        result = connection.execute(text("SELECT 1"))
+        return {"message": "Database Connected Successfully"}
