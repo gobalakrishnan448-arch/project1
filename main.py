e9972ad479305362d838d6816be01a175543f513
Gobala <your_email@gmail.com>
2026-03-05 15:51:38 +0530

    created professional database created

--- main.py
+++ main.py
@@ -7,8 +7,42 @@ app = FastAPI()
 DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL,connect_args={"sslmode":"require"})
 
-@app.get("/test-db")
-def test_db():
+@app.get("/setup-db")
+def setup_db():
     with engine.connect() as connection:
-        result = connection.execute(text("SELECT 1"))
-        return {"message": "Database Connected Successfully"}
+
+        # Colleges Table
+        connection.execute(text("""
+            CREATE TABLE IF NOT EXISTS colleges (
+                id SERIAL PRIMARY KEY,
+                college_name TEXT NOT NULL,
+                district TEXT,
+                college_type TEXT
+            );
+        """))
+
+        # Branches Table
+        connection.execute(text("""
+            CREATE TABLE IF NOT EXISTS branches (
+                id SERIAL PRIMARY KEY,
+                branch_name TEXT NOT NULL
+            );
+        """))
+
+        # Cutoffs Table
+        connection.execute(text("""
+            CREATE TABLE IF NOT EXISTS cutoffs (
+                id SERIAL PRIMARY KEY,
+                college_id INT REFERENCES colleges(id) ON DELETE CASCADE,
+                branch_id INT REFERENCES branches(id) ON DELETE CASCADE,
+                year INT,
+                round INT,
+                community TEXT,
+                quota TEXT,
+                cutoff FLOAT
+            );
+        """))
+
+        connection.commit()
+
+    return {"message": "Professional Tables Created Successfully"}
