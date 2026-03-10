b68b339dfbf759089b2df07574782feb06eba2eb
Gobala <your_email@gmail.com>
2026-03-10 19:54:59 +0530

    fixed database setup

--- main.py
+++ main.py
@@ -16,6 +16,55 @@ engine = create_engine(
 )
 
 # ---------------------------
+# CREATE TABLE
+# ---------------------------
+
+@app.get("/setup-db")
+def setup_db():
+
+    with engine.begin() as connection:
+
+        connection.execute(text("""
+
+        CREATE TABLE IF NOT EXISTS colleges (
+
+            id SERIAL PRIMARY KEY,
+            college_name TEXT,
+            branch TEXT,
+            district TEXT,
+            cutoff FLOAT,
+            community TEXT
+
+        )
+
+        """))
+
+    return {"message": "Table Created"}
+
+
+# ---------------------------
+# INSERT SAMPLE DATA
+# ---------------------------
+
+@app.get("/insert-sample-data")
+def insert_data():
+
+    with engine.begin() as connection:
+
+        connection.execute(text("""
+        INSERT INTO colleges (college_name, branch, district, cutoff, community)
+        VALUES
+        ('PSG College of Technology','CSE','Coimbatore',198,'OC'),
+        ('SSN College of Engineering','CSE','Chennai',197,'OC'),
+        ('Thiagarajar College of Engineering','IT','Madurai',195,'BC'),
+        ('Kongu Engineering College','CSE','Erode',194,'BC'),
+        ('Coimbatore Institute of Technology','CSE','Coimbatore',196,'OC')
+        """))
+
+    return {"message": "Sample Data Inserted"}
+
+
+# ---------------------------
 # PREDICT API
 # ---------------------------
 
@@ -23,7 +72,7 @@ engine = create_engine(
 def predict(cutoff: float, community: str, district: str = "All"):
 
     query = """
-    SELECT college_name, branch, district, cutoff, community
+    SELECT college_name, branch, district, cutoff
     FROM colleges
     WHERE cutoff <= :cutoff
     AND community = :community
@@ -75,24 +124,3 @@ def predict(cutoff: float, community: str, district: str = "All"):
         })
 
     return data
-
-
-# ---------------------------
-# CREATE TABLE
-# ---------------------------
-@app.get("/insert-sample-data")
-def insert_data():
-
-    with engine.begin() as connection:
-
-        connection.execute(text("""
-        INSERT INTO colleges (college_name, branch, district, cutoff, community)
-        VALUES
-        ('PSG College of Technology','CSE','Coimbatore',198,'OC'),
-        ('SSN College of Engineering','CSE','Chennai',197,'OC'),
-        ('Thiagarajar College of Engineering','IT','Madurai',195,'BC'),
-        ('Kongu Engineering College','CSE','Erode',194,'BC'),
-        ('Coimbatore Institute of Technology','CSE','Coimbatore',196,'OC')
-        """))
-
-    return {"message": "Sample Data Inserted"}
